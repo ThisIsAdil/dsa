4226810eb2443d16beabc140336a6fc887cff970
Adil Shaikh <shaikhadil.dev@gmail.com>
2026-04-15 21:42:01 +0530

    completed arrays

--- code.cpp
+++ code.cpp
@@ -1,6 +1,67 @@
 #include <iostream>
 using namespace std;
 
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+}
+
+void changeArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = 2 * arr[i];
+    }
+}
+
+int linearSearch(int arr[], int size, int target)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == target)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+void reverseArray(int arr[], int size)
+{
+    int revArr[size];
+    for (int i = size; i > 0; i--)
+    {
+        revArr[size - i] = arr[i - 1];
+    }
+
+    printArray(revArr, size);
+}
+
+void swapArrayElements(int arr[], int size)
+{
+    // for (int i = size; i > 0 + (size / 2); i--)
+    // {
+    //     swap(arr[i - 1], arr[size - i]);
+    // }
+
+    int start = 0, end = size - 1;
+
+    while (start < end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+
+    printArray(arr, size);
+}
+
 int main()
 {
     int marks[5] = {75, 87, 93, 57, 88};
@@ -33,8 +94,40 @@ int main()
             indexOfL = i;
         }
     }
-    cout << smallest << " at index " << indexOfS << endl;
-    cout << largest << " at index " << indexOfL << endl;
+    // cout << smallest << " at index " << indexOfS << endl;
+    // cout << largest << " at index " << indexOfL << endl;
+
+    // Pass by Reference
+    int arr[] = {11, 34, 99, 8};
+    int size = sizeof(arr) / sizeof(int);
+
+    // changeArray(arr, 3);
+
+    for (int i = 0; i < 3; i++)
+    {
+        // cout << arr[i] << " ";
+    }
+
+    // cout << endl;
+
+    // Linear Search
+    // cout << linearSearch(arr, 3, 99) << endl; // O(n)
+
+    // Reverse an Array
+    // reverseArray(arr, 3);
+
+    // Reverse an Array using swap
+    swapArrayElements(arr, size);
+
+    // HOMEWORK
+
+    // 1. WAF to calculate sum & product of all numbers in an array.
+
+    // 2. WAF to swap the max & min number of an array.
+
+    // 3. WAF to print all the unique values in an array.
+
+    // 4. WAF to print intersection of 2 arrays.
 
     return 0;
 }
